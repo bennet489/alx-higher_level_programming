277630ebacfee657c19d70430314af57f029d534
bennet489 <bennetasare@outlook.com>
2023-08-10 09:59:50 -0700

    0

--- 0x02-python-import_modules/101-easy_print.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print #pythoniscool, followed by a new line,
-# in the standard output.
-# -----------------------------------------------------------
-
-exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
