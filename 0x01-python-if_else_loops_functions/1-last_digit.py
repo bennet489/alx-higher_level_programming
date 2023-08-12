66ab0356349cf3c3d2b14938d2e3e6bc6c48e0e4
bennet489 <bennetasare@outlook.com>
2023-08-12 12:17:08 -0700

    if

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 print('Last digit of', number, 'is', remainder, end=' ')
 if (number > 5):
     print('and is greater than 5')
-elif(number == 0):
+elif (number == 0):
     print('and is 0')
 else:
     print('and is less than 6 and not 0')
