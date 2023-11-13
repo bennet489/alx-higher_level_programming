f845531b31b1392ac977cdac4ca86b5750e9f1c5
bennet489 <bennetasare@outlook.com>
2023-11-13 14:10:58 +0200

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// script that prints the first argument passed to it
-const argv = process.argv[2];
-if (argv === undefined) {
-  console.log('No argument');
-} else if (argv === 1) {
-	console.log('Argument found');
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
+    console.log('No argument');
+} else if (numArgs === 1) {
+    console.log('Argument found');
 } else {
-	console.log('Arguments found');
+    console.log('Arguments found');
 }
+
