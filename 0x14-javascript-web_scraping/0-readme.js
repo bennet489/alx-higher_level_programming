cac3538ad5bda137c555faa0887b507e9559d801
bennet489 <bennetasare@outlook.com>
2024-01-04 11:15:46 -0500

    party

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const process = require('process');
-const filesystem = require('fs');
+// reads and prints contents of a file
+const fs = require('fs');
 
-const file = process.argv[2];
-
-filesystem.readFile(file, 'utf8', function (err, data) {
-  if (err != null) {
+let filename = '';
+if (process.argv[2] !== undefined) {
+  filename = process.argv[2];
+}
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
     console.log(err);
   } else {
     process.stdout.write(data);
