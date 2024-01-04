ad487ee143eea3278dfcd9afc9b238cb9b974351
bennet489 <bennetasare@outlook.com>
2024-01-04 11:10:16 -0500

    Blind

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,11 +3,10 @@ const process = require('process');
 const filesystem = require('fs');
 
 const file = process.argv[2];
+const text = process.argv[3];
 
-filesystem.readFile(file, 'utf8', function (err, data) {
+filesystem.writeFile(file, text, 'utf8', function (err, data) {
   if (err != null) {
     console.log(err);
-  } else {
-    process.stdout.write(data);
   }
 });
